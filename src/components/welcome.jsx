43e7b766fadaa456a1b843cf0f0cce5a3ee21ece
tohshine <tohshine@pop-os.localdomain>
2020-12-05 13:59:24 +0100

    change resume

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -62,7 +62,7 @@ const Welcome = () => {
 
             <div className='flex justify-evenly items-center pt-16  w-full'>
               <a
-                href='https://res.cloudinary.com/dlecos9op/image/upload/v1602742995/kbeweuw6c4zajg5zgwxa.pdf'
+                href='https://res.cloudinary.com/dlecos9op/image/upload/v1607172533/oui3wgrshgxhy7tio0vl.pdf'
                 className='hover:bg-white px-2 py-2 rounded-lg text-orange-800 border-0 '
               >
                 <GetAppIcon />
