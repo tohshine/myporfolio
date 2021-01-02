7f093f922936f86b77809e0e5739a342e44c3165
tohshine <tohshine@pop-os.localdomain>
2021-01-02 16:42:04 +0100

    update mobile app link

--- src/mocks/projects.js
+++ src/mocks/projects.js
@@ -3,44 +3,70 @@ export const project = [
     title: "covid19apps",
     description:
       "This is a covid19 cases tracker,which i built to detect and reveal the number of cases, infected and recovered in my country Nigeria, this web application is build  mainly with react  with day and night mode features to prevent light emission to users.It also have diagnosing test kit features which it still under developement  ",
-    technology: [{id:1, name:"React",url:""},{id:2,name:"Node",url:""}],
+    technology: [
+      { id: 1, name: "React", url: "" },
+      { id: 2, name: "Node", url: "" },
+    ],
     id: 1,
-    image: "https://res.cloudinary.com/dlecos9op/image/upload/v1596791359/auws8aq5nyskngf9d3ih.jpg",
+    image:
+      "https://res.cloudinary.com/dlecos9op/image/upload/v1596791359/auws8aq5nyskngf9d3ih.jpg",
     url: "http://covid19apps.herokuapp.com/",
   },
   {
     title: "Treadme",
     description:
       "Thread is a real-time messaging app group of individual such as developers apprentices,wwhere new technology, skills,library usage is well explained for most developers who lack how to efficiently use such tech. it built with react and runs on nodejs and socket.io is used for real time message tranport ",
-    technology: [{id:1, name:"React",url:""},{id:2,name:"Node",url:""}],
+    technology: [
+      { id: 1, name: "React", url: "" },
+      { id: 2, name: "Node", url: "" },
+    ],
     id: 2,
-    image: "https://res.cloudinary.com/dlecos9op/image/upload/v1596791374/go0hb62xnegnbehqk1lt.jpg",
+    image:
+      "https://res.cloudinary.com/dlecos9op/image/upload/v1596791374/go0hb62xnegnbehqk1lt.jpg",
     url: "http://threadme.herokuapp.com/",
   },
   {
     title: "Fast search",
     description:
       "This software is capable of populating companies ,industrial sectors based on your search and geolocation, this app detect your location and have a precises distance between the user and the company. This web app  built with react and runs on Nodejs and Redux for its state management.  ",
-    technology: [{id:1, name:"React",url:""},{id:2,name:"Node",url:""},{id:3,name:"Redux",url:""}],
+    technology: [
+      { id: 1, name: "React", url: "" },
+      { id: 2, name: "Node", url: "" },
+      { id: 3, name: "Redux", url: "" },
+    ],
     id: 3,
-    image: "https://res.cloudinary.com/dlecos9op/image/upload/v1596791368/fkfws9v05ahcone1rv13.jpg",
+    image:
+      "https://res.cloudinary.com/dlecos9op/image/upload/v1596791368/fkfws9v05ahcone1rv13.jpg",
     url: "http://fastsearch.herokuapp.com/",
   },
   {
-    title: "Covid19 mobile application ",
+    title: "Sellit  mobile application ",
     description:
-      "This is the android version of covid19apps   ",
-    technology: [{id:1, name:"React-native",url:""},{id:2,name:"Node",url:""},{id:3,name:"API",url:""}],
+      "This is the android version for selling agric products that is farmers comes on this application ,upload and sell his farm products and therefore attracts customers who is willing to purchase the product. The beauty of this mobile app is buyers can track the location of the product using Google Maps i.e detects where the product as reached. ",
+    technology: [
+      { id: 1, name: "React-native", url: "" },
+      { id: 2, name: "Node", url: "" },
+      { id: 3, name: "API", url: "" },
+    ],
     id: 4,
-    image: require('../assets/dash.png'),
-    url: "http://fastsearch.herokuapp.com/",
-  },{
+    image: require("../assets/dash.png"),
+    url:
+      "https://drive.google.com/file/d/1djFRtM5h8YR6knQ8ts7cninGCCUco9CW/view?usp=sharing",
+  },
+  {
     title: "Ecommerce application",
     description:
       "A real time Ecommerce web application where buying and selling is carried out. This application lock down item for any individual who orders  until is actually been purchased or product got expired in a timeout window it is built with react as frontend and nodejs as backend which together run inside kubernetes and hosted on digital ocean. p.s this application is not online to avoid the cost of maintaining a kubernetes    ",
-    technology: [{id:1, name:"React",url:""},{id:2,name:"Node",url:""},{id:3,name:"kubernetes",url:""},{name:"Docker",url:""},{name:"NAT",url:""}],
+    technology: [
+      { id: 1, name: "React", url: "" },
+      { id: 2, name: "Node", url: "" },
+      { id: 3, name: "kubernetes", url: "" },
+      { name: "Docker", url: "" },
+      { name: "NAT", url: "" },
+    ],
     id: 5,
-    image: "https://res.cloudinary.com/dlecos9op/image/upload/v1596984198/egsfk939c6m6co2mqa44.png",
+    image:
+      "https://res.cloudinary.com/dlecos9op/image/upload/v1596984198/egsfk939c6m6co2mqa44.png",
     url: "",
   },
 ];
